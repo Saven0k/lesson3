1af7d8085c5b51914af4ba1b0b0edbe3f24ef2c4
Savenok <Romasav2017@gmail.com>
2022-09-07 20:58:31 +0300

    123

--- Program.cs
+++ Program.cs
@@ -1,21 +1,14 @@
-﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
-// A (3,6); B (2,1) -> 5,09
-// A (7,-5); B (1,-1) -> 7,21
-
-//ВАриант не плохой но не продуман - поэтому никак
-// Console.Clear();
-// Console.WriteLine("Введите координату  точкек: ");
-// string numbers = Console.ReadLine();
-// double result = Math. Sqrt(((numbers[3] - numbers[12])*(numbers[3] - numbers[12])) + ((numbers[5] - numbers[14])*(numbers[5] - numbers[14])));
-// Console.WriteLine(result);
-
+﻿// Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
 Console.Clear();
-Console.WriteLine("Введите координату A-x: ");
-int Ax = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату A-y: ");
-int Ay = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату b-x: ");
-int Bx = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату b-y: ");
-int By = int.Parse(Console.ReadLine());
-Console.WriteLine(Math.Round(Math.Sqrt((Math.Pow((Ax-Bx) , 2)) + (Math.Pow((Ay-By), 2))) , 3 ));
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int a = 1;
+Console.Write($"{a} -> ");
+while(a<number)
+{
+    Console.Write($"{Math.Pow((a) , 2)} , ");
+    a++;
+}
+Console.Write($"{Math.Pow((number) , 2)  }. ");
