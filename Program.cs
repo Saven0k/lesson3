a9834e2fdd1de1cf7ee35599ca6640a64ac74af9
Savenok <Romasav2017@gmail.com>
2022-09-09 16:43:55 +0300

    21.2.0

--- Program.cs
+++ Program.cs
@@ -1,21 +1,19 @@
-﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
-// A (3,6); B (2,1) -> 5,09
-// A (7,-5); B (1,-1) -> 7,21
-
-//ВАриант не плохой но не продуман - поэтому никак
-// Console.Clear();
-// Console.WriteLine("Введите координату  точкек: ");
-// string numbers = Console.ReadLine();
-// double result = Math. Sqrt(((numbers[3] - numbers[12])*(numbers[3] - numbers[12])) + ((numbers[5] - numbers[14])*(numbers[5] - numbers[14])));
-// Console.WriteLine(result);
-
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 Console.Clear();
 Console.WriteLine("Введите координату A-x: ");
 int Ax = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите координату A-y: ");
 int Ay = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату b-x: ");
+Console.WriteLine("Введите координату A-z: ");
+int Az = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату B-x: ");
 int Bx = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите координату b-y: ");
+Console.WriteLine("Введите координату B-y: ");
 int By = int.Parse(Console.ReadLine());
-Console.WriteLine(Math.Round(Math.Sqrt((Math.Pow((Ax-Bx) , 2)) + (Math.Pow((Ay-By), 2))) , 3 ));
+Console.WriteLine("Введите координату B-z: ");
+int Bz = int.Parse(Console.ReadLine());
+double result = (Math.Pow((Ax-Bx), 2)) + (Math.Pow((Ay-By), 2)) + (Math.Pow((Az-Bz), 2));
+Console.WriteLine(Math.Round(Math.Sqrt(result) , 2));
